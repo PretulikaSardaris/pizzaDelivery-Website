1afae94920a94163bd71a3e01385068f166c4d21
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-05-20 07:43:44 +0200

    new done

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,14 +5,14 @@ import Price from '@/components/Price'
 
 const SingleProductPage = () => {
   return (
-    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-orange-500 md:flex-row md:items-center'>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-orange-500 md:flex-row md:gap-8 md:items-center'>
       {/* imagecontainer  */}
-      {singleProduct.img && (<div className='relative w-full h-1/2'>
+      {singleProduct.img && (<div className='relative w-full h-1/2 md:h-[70%]'>
         <Image src={singleProduct.img} className='object-contain' alt='img' fill></Image>
       </div>)}
       {/* text container  */}
-      <div className='h-1/2 flex flex-col gap-4'>
-        <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
+      <div className='h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8'>
+        <h1 className='text-3xl font-bold uppercase xl:text-5xl'>{singleProduct.title}</h1>
         <p className=''>{singleProduct.desc}</p>
         <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
         
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+
+"use client"
+import React, { useEffect, useState } from 'react'
 type Props = {
     price:number;
     id:number;
@@ -8,12 +10,29 @@ type Props = {
 }
 
 const Price = ({price, id, options} : Props) => {
+const [total, setTotal] = useState(price);
+const[quantity , setQuantity] = useState(1);
+const[selected, setSelected] = useState(0);
+
+useEffect (() => {
+setTotal(
+  quantity * (options ? price + options[selected].additionalPrice : price)
+)
+} , [quantity , selected , price , options])
+
+
   return (
     <div className='flex flex-col gap-4 '>
-      <h2 className='font-bold text-2xl'>${price}</h2>
+      <h2 className='font-bold text-2xl'>${total.toFixed(2)}</h2>
       <div className='flex gap-4'>
-        {options?.map(option =>(
-          <button className='p-2 ring-2 ring-orange-500 rounded-md hover:bg-orange-500 hover:text-white' key={option.title}>{option.title}</button>
+        {options?.map((option , index) =>(
+          <button className='min-w-[6rem] p-2 ring-2 ring-orange-500 rounded-md '
+          style={{
+            background: selected === index ? "rgb(249 115 22)" : "white",
+            color: selected === index ? " white" : "orange"
+          }}
+          onClick={() => setSelected(index)}
+          key={option.title}>{option.title}</button>
         ))}
       </div>
       {/* quantity and button container  */}
@@ -21,9 +40,9 @@ const Price = ({price, id, options} : Props) => {
       <div className='flex justify-between w-full ring-2 ring-orange-500 rounded-sm p-3 hover:bg-fuchsia-50'>
         <span>Quantity</span>
         <div className='flex gap-2 items-center'>
-          <button>{'<'}</button>
-          <span>1</span>
-          <button>{'>'}</button>
+          <button onClick={ () => setQuantity(prev => (prev > 1 ? prev - 1 : 1))}>{'<'}</button>
+          <span>{quantity}</span>
+          <button onClick={ () => setQuantity(prev => (prev < 9 ? prev + 1 : 9))}>{'>'}</button>
         </div>
       </div>
       <button className='uppercase w-56 bg-orange-500 text-white p-3 ring-2 ring-orange-600 rounded-sm'>Add to cart</button>
